18c506c3f82ed6487270fd4e4a04e038679ef113
muhammadalimurtadho05 <muhammadalimurtadho2209051@gmail.com>
2026-05-29 20:45:01 +0700

    feat :fix-calc

--- bayes_classifier/settings.py
+++ bayes_classifier/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zq1bthwq!uj811t=rb8@#y*&x9d0c+vvy(ojlnbs%-jr^2#dtk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
