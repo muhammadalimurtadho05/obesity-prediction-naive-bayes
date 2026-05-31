d19e6d2ec64efe75959f0ce453468e352261b322
Yohanes Oktanio <yohanesoktayt72@gmail.com>
2026-06-01 01:05:03 +0700

    fix: vercel builds | aduhai ini kenapa semua package di komputermu masuk ke sini semua wkwkwk

--- bayes_classifier/settings.py
+++ bayes_classifier/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
+                'whitenoise.middleware.WhiteNoiseMiddleware',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -116,3 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'classifier' / 'static',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
